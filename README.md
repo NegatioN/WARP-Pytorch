4d2464d0ed567bbcc08dbe2d7e06a333d829f61d
NegatioN <joakimrishaug@gmail.com>
2018-07-12 13:54:29 +0200

    init warp loss repo

---
+++ README.md
@@ -0,0 +1,20 @@
+# WARP loss for PyTorch
+
+An implementation of WARP loss which uses matrixes and stays on the GPU in PyTorch.
+
+This means instead of using a for-loop to find the first offending negative sample that ranks above our positive,
+we compute all of them at once. Only later do we find which sample is the first offender, and compute the loss with
+respect to this sample.
+
+The advantage is that it can use the speedups that comes with GPU-usage. 
+
+## Assumptions
+The loss function assumes you have already sampled your negatives randomly.
+
+As an example this could be done in your dataloader:
+
+1. Assume we have a total dataset of 100 items
+2. Select a positive sample with index 8
+2. Your negatives should be a random selection from 0-100 excluding 8.
+
+Ex input to loss function: model scores for pos: [8] neg: [88, 3, 99, 7]
---
+++ warp_loss.py
@@ -0,0 +1,47 @@
+import torch
+
+def num_tries_gt_zero(scores, batch_size, max_trials, max_num, device):
+    '''
+    scores: [batch_size x N] float scores
+    returns: [batch_size x 1] the lowest indice per row where scores were first greater than 0. plus 1
+    '''
+    tmp = scores.gt(0).nonzero().t()
+    # We offset these values by 1 to look for unset values (zeros) later
+    values = tmp[1] + 1
+    # TODO just allocate normal zero-tensor and fill it?
+    # Sparse tensors can't be moved with .to() or .cuda() if you want to send in cuda variables first
+    if device.type == 'cuda':
+        t = torch.cuda.sparse.LongTensor(tmp, values, torch.Size((batch_size, max_trials+1))).to_dense()
+    else:
+        t = torch.sparse.LongTensor(tmp, values, torch.Size((batch_size, max_trials+1))).to_dense()
+    t[(t == 0)] += max_num # set all unused indices to be max possible number so its not picked by min() call
+
+    tries = torch.min(t, dim=1)[0]
+    return tries
+
+
+def warp_loss(positive_predictions, negative_predictions, num_labels, device):
+    '''
+    positive_predictions: [batch_size x 1] floats between -1 to 1
+    negative_predictions: [batch_size x N] floats between -1 to 1
+    num_labels: int total number of labels in dataset (not just the subset you're using for the batch)
+    '''
+    batch_size, max_trials = negative_predictions.size(0), negative_predictions.size(1)
+
+    offsets, ones, max_num = (torch.arange(0, batch_size, 1).long().to(device) * (max_trials + 1),
+                              torch.ones(batch_size, 1).float().to(device),
+                              batch_size * (max_trials + 1) )
+
+    sample_scores = (1 + negative_predictions - positive_predictions)
+    # Add column of ones so we know when we used all our attempts, This is used for indexing and computing should_count_loss if no real value is above 0
+    sample_scores, negative_predictions = (torch.cat([sample_scores, ones], dim=1),
+                                           torch.cat([negative_predictions, ones], dim=1))
+
+    tries = num_tries_gt_zero(sample_scores, batch_size, max_trials, max_num, device)
+    attempts, trial_offset = tries.float(), (tries - 1) + offsets
+    loss_weights, should_count_loss = ( torch.log(torch.floor((num_labels - 1) / attempts)),
+                                        (attempts <= max_trials).float()) #Don't count loss if we used max number of attempts
+
+    losses = loss_weights * ((1 - positive_predictions.view(-1)) + negative_predictions.view(-1)[trial_offset]) * should_count_loss
+
+    return losses.sum()
